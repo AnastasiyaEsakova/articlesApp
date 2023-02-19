e8ff41dd39a0ef7ffcae4759445318c14daa97ba
Anastasiya Esakova <nastya77esakova@yandex.ru>
2023-02-19 16:07:13 +0300

    fix: jest

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,21 @@
+import { classNames } from "./classNames";
+
+describe("classNames", () => {
+  test("with one class", () => {
+    expect(classNames("class")).toBe("class");
+  });
+
+  test("with additional class", () => {
+    const expected = "class additionalClass1 additionalClass2";
+    expect(
+      classNames("class", {}, ["additionalClass1", "additionalClass2"])
+    ).toBe(expected);
+  });
+
+  test("with mods", () => {
+    const expected = "class modClass1";
+    expect(classNames("class", { modClass1: true, modClass2: false })).toBe(
+      expected
+    );
+  });
+});
